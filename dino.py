e75b85a480eba6fb511c33371a0b8de1a04c4d8c
Pomelo <daniildikunov22@gmail.com>
2025-01-27 17:53:09 +0300

    dino.py

--- dino.py
+++ dino.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint, choice
 import pickle
+
 pygame.init()
 
 WIDTH, HEIGHT = 1000, 400
@@ -9,7 +10,8 @@ FPS = 60
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-def scoresSave(): #сохраняем очки
+
+def scoresSave():  # сохраняем очки
     global scoresBest
 
     if scores > scoresBest:
@@ -17,6 +19,7 @@ def scoresSave(): #сохраняем очки
         pickle.dump(scores, f)
         f.close()
         scoresBest = scores
+
 
 def scoresLoad():
     global scoresBest
@@ -26,6 +29,7 @@ def scoresLoad():
         f.close()
     except:
         pass
+
 
 imgSprites = pygame.image.load('sprites.png').convert_alpha()
 imgBG = imgSprites.subsurface(2, 104, 2400, 26)
@@ -40,7 +44,7 @@ imgCactus = [imgSprites.subsurface(446, 2, 34, 70),
              imgSprites.subsurface(512, 2, 68, 70),
              imgSprites.subsurface(652, 2, 50, 100),
              imgSprites.subsurface(752, 2, 98, 100),
-             imgSprites.subsurface(850, 2, 102, 100),]
+             imgSprites.subsurface(850, 2, 102, 100), ]
 imgPter = [imgSprites.subsurface(260, 0, 92, 82),
            imgSprites.subsurface(352, 0, 92, 82)]
 imgRestart = imgSprites.subsurface(2, 2, 72, 64)
@@ -57,11 +61,13 @@ speed = 10
 frame = 0
 scores = 0
 scoresBest = 0
-level = 0
+uruven = 0
+night = 0
 
 bgs = [pygame.Rect(0, HEIGHT - 50, 2400, 26)]
 objects = []
 timer = 0
+
 
 class Obj:
     def __init__(self):
@@ -76,7 +82,7 @@ class Obj:
             self.speed = 0
             py = HEIGHT - 20
 
-        self.rect = self.image[0].get_rect(bottomleft = (WIDTH, py))
+        self.rect = self.image[0].get_rect(bottomleft=(WIDTH, py))
         self.frame = 0
 
     def update(self):
@@ -89,7 +95,6 @@ class Obj:
             timer = 60
             sy = -10
             sndGameOver.play()
-            scoresSave()
 
         if self.rect.right < 0: objects.remove(self)
 
@@ -110,10 +115,11 @@ while play:
     pressJump = keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP] or b1
     pressSit = keys[pygame.K_LCTRL] or keys[pygame.K_s] or keys[pygame.K_DOWN] or b3
 
-    if (pressJump or pressSit) and speed == 0 and timer == 0: #кнопка
+    if (pressJump or pressSit) and speed == 0 and timer == 0:  # кнопка
+        scoresSave()
         py, sy, = 380, 0
         isStand = False
-        speed = 10 #значения на ноль
+        speed = 10  # значения на ноль
         frame = 0
         scores = 0
         objects = []
@@ -130,14 +136,17 @@ while play:
     if py > HEIGHT - 20:
         py, sy, isStand = HEIGHT - 20, 0, True
 
-    if speed == 0: dinoImage = imgDinoLose[0]
-    elif pressSit: dinoImage = imgDinoSit[int(frame)]
-    else: dinoImage = imgDinoStand[int(frame)]
+    if speed == 0:
+        dinoImage = imgDinoLose[0]
+    elif pressSit:
+        dinoImage = imgDinoSit[int(frame)]
+    else:
+        dinoImage = imgDinoStand[int(frame)]
 
     dw, dh = dinoImage.get_width(), dinoImage.get_height()
     dinoRect = pygame.Rect(150, py - dh, dw, dh)
 
-    for i in range(len(bgs)-1, -1, -1):
+    for i in range(len(bgs) - 1, -1, -1):
         bg = bgs[i]
         bg.x -= speed
 
@@ -148,21 +157,25 @@ while play:
 
     for obj in objects: obj.update()
 
-    if timer > 0: timer -= 1
+    if timer > 0:
+        timer -= 1
     elif speed > 0:
         timer = randint(100, 150)
         Obj()
 
     scores += speed / 50
 
-    if scores // 100 > level: #вычисление
-        level = scores // 100
+    if scores // 100 > uruven:  # вычисление
+        uruven = scores // 100
         sndLevelup.play()
 
     if speed > 0:
         speed = 10 + scores // 100
 
-    window.fill('white')
+    night = (night + 0.02) % 512
+
+    d = abs(night - 256)
+    window.fill((d, d, d))
     for bg in bgs: window.blit(imgBG, bg)
     for obj in objects: obj.draw()
     window.blit(dinoImage, dinoRect)
@@ -174,11 +187,10 @@ while play:
     window.blit(text, (50, 10))
 
     if speed == 0 and timer == 0:
-        rect = imgRestart.get_rect(center = (WIDTH // 2, HEIGHT // 2))
+        rect = imgRestart.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         window.blit(imgRestart, rect)
 
     pygame.display.update()
     clock.tick(FPS)
-
+scoresSave()  # сохраняем результат
 pygame.quit()
-scoresSave() #сохраняем результат
